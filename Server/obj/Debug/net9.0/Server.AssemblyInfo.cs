4271244798a46aa533d5cd13746ad4a0ba3211d8
Vikash Verma <130314349+Vikashverma2@users.noreply.github.com>
2025-09-11 21:27:24 +0530

    server: add some conditions and mongodb

--- Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e01964bb85b69add5b3180c0785ee93d8c05587")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+465e7b2bffc0e04308ada36bfa8ff6d65b985e4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
